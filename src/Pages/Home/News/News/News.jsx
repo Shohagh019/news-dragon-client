bd56457a0a6b7ea29ce9ca6dc4eceea1e39a04ab
Shohagh019 <shohaghmunna@gmail.com>
2023-05-22 23:40:32 -0700

    added

--- src/Pages/Home/News/News/News.jsx
+++ src/Pages/Home/News/News/News.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link, useLoaderData } from "react-router-dom";
+import EditorInsight from "../EditorInsight/EditorInsight";
 
 const News = () => {
+  const news = useLoaderData();
+  const { title, details, image_url, category_id } = news;
   return (
     <div>
-      <h2>News Details Comming Soon...</h2>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="danger">
+              <FaArrowLeft></FaArrowLeft> All News In this Category
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      <EditorInsight></EditorInsight>
     </div>
   );
 };
